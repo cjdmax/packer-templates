37b5e57b650bf3274e4044b705f11281a8ba05b9
Adrian van Dongen <github@goodfellasonline.nl>
2014-10-29 13:07:04 +0100

    Add new ubuntu precise template with puppet 2.7.26.

---
+++ ubuntu-12.04.5-64bit-vbox-4.3.18-puppet-2.7.26/tests/spec/base_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+## Packages
+
+%w(sysstat curl unzip vim multitail htop atop ngrep tcpdump git).each do |package|
+  describe package(package) do
+    # Check if all packages are installed.
+    it { should be_installed }
+  end
+end
+
+## Files and directories
+
+describe file('/etc/vagrant_box_build_time') do
+  # file containing build time should be created
+  it { should be_file }
+end
+
+describe file('/etc/default/grub') do
+    # Grub timeout should be 0.
+    its(:content) { should match /GRUB_TIMEOUT=0/ }
+end
+
+describe file('/etc/network/interfaces') do
+    # Verify 2 sec delay to the interface up.
+    its(:content) { should match /pre-up sleep 2/ }
+end

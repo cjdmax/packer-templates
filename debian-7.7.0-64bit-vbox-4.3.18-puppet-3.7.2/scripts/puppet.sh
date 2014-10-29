76545499b07996fbe2a9f196480b46843c5c44c7
Adrian van Dongen <github@goodfellasonline.nl>
2014-10-29 12:58:19 +0100

    Add new debian wheezy template with puppet 3.7.2.

---
+++ debian-7.7.0-64bit-vbox-4.3.18-puppet-3.7.2/scripts/puppet.sh
@@ -0,0 +1,16 @@
+## Install puppet.
+
+# Download and install "release” package.
+puppet_release="puppetlabs-release-wheezy.deb"
+wget -q http://apt.puppetlabs.com/${puppet_release}
+dpkg -i $puppet_release
+rm $puppet_release
+
+# Update apt.
+apt-get update
+
+# Install ruby and rubygems.
+apt-get install -y ruby rubygems
+
+# Install Puppet.
+apt-get install -y puppet

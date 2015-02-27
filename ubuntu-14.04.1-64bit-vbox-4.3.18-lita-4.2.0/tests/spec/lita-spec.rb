529c82f0fbf457e50330149ee78ac54917516f72
Adrian van Dongen <adrian@kumina.nl>
2015-02-27 13:03:03 +0100

    Add new ubuntu trusty template with lita 4.2.0

---
+++ ubuntu-14.04.1-64bit-vbox-4.3.18-lita-4.2.0/tests/spec/lita-spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+## Packages
+
+describe package('ruby2.0') do
+  # Package ruby2.0 should be installed.
+  it { should be_installed }
+end
+
+describe package('ruby2.0-dev') do
+  # Package ruby2.0-dev should be installed.
+  it { should be_installed }
+end
+
+describe command("gem --version") do
+  # Check if gem can be executed
+  it { should_not return_stderr /command not found/ }
+end
+
+describe command("lita --version") do
+  # Check if Lita can be executed
+  it { should_not return_stderr /command not found/ }
+end

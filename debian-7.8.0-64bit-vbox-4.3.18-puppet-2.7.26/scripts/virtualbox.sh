aeba6575986986ecb8810acebb8a06bd19e7e02f
Liam Macgillavry <liam@kumina.nl>
2015-03-24 17:00:18 +0100

    new boxes: debian 780 for vbox and qemu

---
+++ debian-7.8.0-64bit-vbox-4.3.18-puppet-2.7.26/scripts/virtualbox.sh
@@ -0,0 +1,13 @@
+## Install Virtualbox Guest Additions.
+
+## Check for vbox version.
+if test -f .vbox_version ; then
+
+  # Install virtualbox guest additions.
+  echo "Installing virtualbox:"
+  cat .vbox_version
+  mount -o loop VBoxGuestAdditions.iso /media/cdrom
+  sh /media/cdrom/VBoxLinuxAdditions.run
+  umount /media/cdrom
+
+fi
